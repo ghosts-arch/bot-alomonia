e21bfb6dcf8dd88a36e088fe6c9df33cf6a11cc2
ghosts-arch <ronan.questier@mailfence.com>
2024-12-08 03:00:30 +0100

    add category in today advent calendar box & split emotes & add IA usage mention

--- src/core/embeds/embed.py
+++ src/core/embeds/embed.py
@@ -1,5 +1,5 @@
 # coding : utf-8
-# Python 3.10
+# Python 3.10
 # ----------------------------------------------------------------------------
 
 import discord
@@ -13,15 +13,19 @@ class Embed(discord.Embed):
         description: str | None = None,
         color: int | discord.Color | None = None,
         url: str | None = None,
+        footer: str | None = None,
     ) -> None:
 
         super().__init__(
-            title = title,
-            description = description,
-            color = color if color is not None else discord.Color.purple(),
-            url = url,
+            title=title,
+            description=description,
+            color=color if color is not None else discord.Color.purple(),
+            url=url,
         )
 
     def set_description(self, description: str):
         self.description = description
         return self
+
+    def set_footer(self, text=None, icon_url=None):
+        return super().set_footer(text=text, icon_url=icon_url)
--- src/core/managers/today_advent_calendar_box.py
+++ src/core/managers/today_advent_calendar_box.py
@@ -20,8 +20,15 @@ class TodayAdventCalendarBoxManager(Manager):
             )
         except Exception as err:
             return ErrorEmbed(description=f"{err}")
-        # #selfcare (test) - a laisser pour l'instant
-        advent_calendar_channel = await self.__client.fetch_channel(1308571886267138088)
+
+        if self.__client.config.get("MODE") == "production":
+            advent_calendar_channel = await self.__client.fetch_channel(
+                self.__client.config.get("ADVENT_CALENDAR_CHANNEL_ID")
+            )
+        else:
+            advent_calendar_channel = await self.__client.fetch_channel(
+                self.__client.config.get("TEST_CHANNEL_ID")
+            )
 
         if not advent_calendar_channel:
             raise Exception("recurent_channel is None")
@@ -29,10 +36,23 @@ class TodayAdventCalendarBoxManager(Manager):
         if not isinstance(advent_calendar_channel, discord.TextChannel):
             raise Exception("birthdays_channel is not a TextChannel")
 
+        match today_advent_calendar_box.category:
+            case "movie":
+                category = "🎬 Film"
+            case "music":
+                category = "🎵 Musique"
+            case "video_game":
+                category = "🎮 Jeu vidéo"
+            case "_":
+                print("error : unvalid category")
+                return
+
         await advent_calendar_channel.send(
             content="@everyone",
             embed=Embed(
                 title=f"Jour {today_advent_calendar_box.day} du calendrier de l'avant de Alomonia",
-                description=f"Voilà l'indice du jour : || {today_advent_calendar_box.clues} ||. \nA vous de jouer !",
+                description=f"Voilà les indices du jour :\n\n\t Catégorie : || {category} || \n\n Emojis : {" ".join([f"||{c}||" for c in list(today_advent_calendar_box.clues)])}\n\nA vous de jouer !",
+            ).set_footer(
+                text="Les énigmes du calendrier de l'avent sont générées par ChatGPT."
             ),
         )
